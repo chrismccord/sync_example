af67b83c366cd93a11b0de4ff58afb6f6ec39397
Chris McCord <chris@chrismccord.com>
2013-04-19 19:55:00 -0400

    Add Styling and clean up layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 SyncExample::Application.routes.draw do
+  
   devise_for :users
+
+  devise_scope :user do 
+    authenticated :user do
+      root to: 'projects#index'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new'
+    end
+  end
 
   resources :projects do
     resources :todos do
       resources :comments
     end
   end
-
-
   resources :users
-
-  root to: 'pages#index'
-  get 'update', to: 'pages#update'
 end
