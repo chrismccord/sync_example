e773beee70c730d4054b15f9c0f03c3c297958e0
Felix Bünemann <buenemann@louis.info>
2013-12-22 02:16:42 +0100

    Fix deprecation: secret_token => secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SyncExample::Application.config.secret_token = '8aed32b6f71804fdf73ef05f6a455732202054143068616ba8e4e94b8fa41192ab8f60d8b52bd3cdd3f02c6296309e2ea5b2ceeb706bceb268e38d3bbe98ccf4'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SyncExample::Application.config.secret_key_base = '8aed32b6f71804fdf73ef05f6a455732202054143068616ba8e4e94b8fa41192ab8f60d8b52bd3cdd3f02c6296309e2ea5b2ceeb706bceb268e38d3bbe98ccf4'
