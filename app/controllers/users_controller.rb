5a5c99f7d29ee2bdab7d45ddaf60259b3977e277
wursttheke <philipp@caution-media.de>
2014-03-04 08:55:14 +0100

    permit email, password on users to create new ones

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :password)
   end
 end
